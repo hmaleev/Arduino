1f2c43bdca5f0e80ca7b51681f788edae640c69e
Hristo <hmaleev@gmail.com>
2017-06-04 14:30:16 +0300

    Bluetooth now uses softaware serial lib

--- BluetoothTemperature/BluetoothTemperature.ino
+++ BluetoothTemperature/BluetoothTemperature.ino
@@ -2,23 +2,38 @@
 // - Adafruit Unified Sensor Library: https://github.com/adafruit/Adafruit_Sensor
 // - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
 
+/*
+ The circuit:
+ * RX is digital pin 10 (connect to TX of other device)
+ * TX is digital pin 11 (connect to RX of other device)
+
+
+*/
+
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <SoftwareSerial.h>
 
 #define DHTPIN            2         // Pin connected to the DHT sensor.
 #define DHTTYPE           DHT21     // DHT 21 (AM2301)
+SoftwareSerial BT(10, 11);    // RX, TX
 
 DHT_Unified dht(DHTPIN, DHTTYPE);
 uint32_t delayMS;
 
-// Bluetooth module HC-06 is connected on pins 0(RX) and 1(TX). 
+// Bluetooth module HC-06 is connected on pins 10(RX) and 11(TX). 
 //This means that data passed to Serial.print() function is sent via bluetooth to other devices
 
 void setup() {
   
   Serial.begin(9600); 
   // Initialize device.
+   while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  BT.begin(9600);
+
   dht.begin();
   sensor_t sensor;
   dht.temperature().getSensor(&sensor);
@@ -54,9 +69,18 @@ void loop() {
   }
 
   Serial.print(temperature);
-  Serial.print("*C");
-  Serial.print("/");
+  Serial.print("");
+  Serial.print("|");
   Serial.print(humidity);
-  Serial.println("%");  
+  Serial.print("|");  
+  Serial.println("");  
+
+  
+  BT.print(temperature);
+  BT.print("");
+  BT.print("|");
+  BT.print(humidity);
+  BT.print("|");
+  BT.println(""); 
 }
 
